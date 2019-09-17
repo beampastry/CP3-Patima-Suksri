7d1d48ac4b2d2337aa2a41e1d7bc6380dc9b1332
beampastry <55445917+beampastry@users.noreply.github.com>
2019-09-17 20:24:06 +0700

    Add files via upload

---
+++ Exercise4_Patima_S.py
@@ -0,0 +1,9 @@
+FE = 68.7
+GB = 80.2
+ICS = 78.3
+CP = 58.5
+print("---Your Score---")
+print("Foundation English : ",FE)
+print("General Business : ",GB)
+print("Introduction to Computer Systems : ",ICS)
+print("Computer Programming : ",CP)
